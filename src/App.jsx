ef778a6e87e34f6c49b071e89dc38a85c8968614
gwolf-dev <gabrielw.ttm@gmail.com>
2024-12-31 18:45:53 -0300

    feat: add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+import { BrowserRouter, Routes, Route } from 'react-router';
+
+import { Home, Login, PageNotFound, Register } from './template';
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
