7166a4b82a67739cb8f2f7e1ad303c8b89fcef67
gwolf-dev <gabrielw.ttm@gmail.com>
2024-12-31 18:14:43 -0300

    feat: ag-grid config

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
 function App() {
   return <div>Hello World!</div>;
 }
